3aa3775aa72f8ceb7cd18141fdd924b0a8140b5e
ilham saleh <ilham2saleh@gmail.com>
2024-06-24 15:40:45 +0100

    Set up next.js and install shadcn

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import AuthScreen from "@/components/home/auth-screen/AuthScreen";
+import HomeScreen from "@/components/home/home-screen/HomeScreen";
+
+export default function Home() {
+  const user = false;
+
+  return <main>{user ? <HomeScreen /> : <AuthScreen />}</main>;
+}
---
+++ src/components/home/auth-screen/AuthScreen.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AuthScreen = () => {
+  return <div className="text-primary">AuthScreen</div>;
+};
+
+export default AuthScreen;

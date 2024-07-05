00ebea8b97021a0616ea7ae56fec21d3e10b058d
ilham saleh <ilham2saleh@gmail.com>
2024-07-05 18:41:04 +0100

    Build Sidebar

--- src/components/home/home-screen/HomeScreen.tsx
+++ src/components/home/home-screen/HomeScreen.tsx
@@ -1,14 +1,14 @@
 "use client";
+import BaseLayout from "@/components/BaseLayout";
 import { ModeToggle } from "@/components/ModeToggle";
 import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs";
 import React from "react";
 
 const HomeScreen = () => {
   return (
-    <div>
-      <ModeToggle />
-      <LogoutLink>Logout</LogoutLink>
-    </div>
+    <BaseLayout>
+      <div>Home Screen</div>
+    </BaseLayout>
   );
 };
 
